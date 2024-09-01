d7fba556ba9953ccefc2738c25568e7f95a5b382
abdalrhmanaboalsoud <abdalrhmanwaleed1999@gmail.com>
2024-09-02 01:07:22 +0300

    Describe your changes here

--- app/page.js
+++ app/page.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import About from "./components/About/About";
 import HomePage from "./components/HomePage/HomePage";
 import LoginPage from "./components/Authantication/Login";
+import LOC from "./components/LOC/LOC";
 
 
 
@@ -11,6 +12,7 @@ export default function Home() {
     <>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <Header />
+    <LOC />
     {/* <HomePage /> */}
     <LoginPage />
     <Footer />
