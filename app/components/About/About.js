639972fa5485fc3caa1d9b8b71c9e211b3f6c396
Yaman-Katalan <23040456@student.ltuc.com>
2024-09-02 14:26:45 +0000

    abc

--- app/components/About/About.js
+++ app/components/About/About.js
@@ -1,9 +1,120 @@
 import "./About.css";
+import Image from "next/image";
 
 export default function About() {
   return (
     <>
-      <h1 className="container">About Us Page!!!</h1>
+      <div className="about-us">
+        <h2 className="main-title">
+          Team Members<span></span>
+        </h2>
+        <div className="team-members">
+          <div className="container">
+
+
+            <div className="member">
+
+              <div className="pic">
+                <Image
+                  src="/images/team_member.jpeg"
+                  width={100}
+                  height={100}
+                  alt="img"
+                />
+                <div className="helper">
+                  <i class="ri-linkedin-box-fill one"></i>
+                  <i class="ri-github-fill two"></i>
+                  <i class="ri-facebook-circle-fill three"></i>
+                  <i class="ri-slack-line four"></i>
+                </div>
+              </div>
+
+              <div className="info">
+                <h3>Yaman Katalan</h3>
+                <p>Full-Stack Web Developer</p>
+              </div>
+
+            </div>
+
+
+            <div className="member">
+
+              <div className="pic">
+                <Image
+                  src="/images/team_member.jpeg"
+                  width={100}
+                  height={100}
+                  alt="img"
+                />
+                <div className="helper">
+                  <i class="ri-linkedin-box-fill one"></i>
+                  <i class="ri-github-fill two"></i>
+                  <i class="ri-facebook-circle-fill three"></i>
+                  <i class="ri-slack-line four"></i>
+                </div>
+              </div>
+
+              <div className="info">
+                <h3>Member Name</h3>
+                <p>Full-Stack</p>
+              </div>
+
+            </div>
+
+
+            <div className="member">
+
+              <div className="pic">
+                <Image
+                  src="/images/team_member.jpeg"
+                  width={100}
+                  height={100}
+                  alt="img"
+                />
+                <div className="helper">
+                  <i class="ri-linkedin-box-fill one"></i>
+                  <i class="ri-github-fill two"></i>
+                  <i class="ri-facebook-circle-fill three"></i>
+                  <i class="ri-slack-line four"></i>
+                </div>
+              </div>
+
+              <div className="info">
+                <h3>Member Name</h3>
+                <p>Full-Stack</p>
+              </div>
+
+            </div>
+
+
+            <div className="member">
+
+              <div className="pic">
+                <Image
+                  src="/images/team_member.jpeg"
+                  width={100}
+                  height={100}
+                  alt="img"
+                />
+                <div className="helper">
+                  <i class="ri-linkedin-box-fill one"></i>
+                  <i class="ri-github-fill two"></i>
+                  <i class="ri-facebook-circle-fill three"></i>
+                  <i class="ri-slack-line four"></i>
+                </div>
+              </div>
+
+              <div className="info">
+                <h3>Member Name</h3>
+                <p>Full-Stack</p>
+              </div>
+
+            </div>
+
+
+          </div>
+        </div>
+      </div>
     </>
   );
 }
